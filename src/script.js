68cac9956ae661a59848f0ab3e669ec513fcab23
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-03-03 21:09:00 +0100

    Injected icon image using the API.
    small CSS changes

--- src/script.js
+++ src/script.js
@@ -1,13 +1,20 @@
 function refreshWeather(response){
  let temperatureElement = document.querySelector("#current-temperature");
  let temperature = response.data.temperature.current;
- 
+ console.log(response.data);
   let cityElement= document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+
+  let iconElement = document.querySelector("#icon");
+  
+  icon.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
 
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
@@ -48,3 +55,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Papendrecht");
+
